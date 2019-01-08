a335cddf78775990038a3db50ec893acbc425881
Phuc Chau <apple@STS-PHUCCHAU.local>
2019-01-08 10:06:38 +0700

    remove android and ios folder

--- ios/Classes/XliveSwitchPlugin.h
+++
@@ -1,4 +0,0 @@
-#import <Flutter/Flutter.h>
-
-@interface XliveSwitchPlugin : NSObject<FlutterPlugin>
-@end
--- ios/Classes/XliveSwitchPlugin.m
+++
@@ -1,8 +0,0 @@
-#import "XliveSwitchPlugin.h"
-#import <xlive_switch/xlive_switch-Swift.h>
-
-@implementation XliveSwitchPlugin
-+ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftXliveSwitchPlugin registerWithRegistrar:registrar];
-}
-@end
